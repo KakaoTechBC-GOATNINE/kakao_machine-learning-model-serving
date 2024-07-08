c5a2210ac007ed47d0b0279b66bd1dca9aea8626
강우빈 <kangwoobin@gang-ubin-ui-MacBookPro.local>
2024-07-08 15:41:11 +0900

    feat : 프로젝트 초기 구성

---
+++ server/src/main/java/com/example/kakao_mlms/KakaoMlmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.kakao_mlms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoMlmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoMlmsApplication.class, args);
+	}
+
+}
