ea7c5249554c12b8d9d8ccdc0d34aa5efe82891b
JJ Lee <maiload77@gmail.com>
2024-07-14 05:05:43 +0900

    #13 fix : Exact 매칭에서 Like 매칭으로 변경

--- server/src/main/java/com/example/kakao_mlms/repository/UserRepository.java
+++ server/src/main/java/com/example/kakao_mlms/repository/UserRepository.java
@@ -19,7 +19,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "update User u set u.refreshToken = :refreshToken where u.id = :userId")
     void updateRefreshToken(@Param("userId") Long userId, @Param("refreshToken") String refreshToken);
 
-    Page<User> findByNickname(String nickname, Pageable pageable);
+    Page<User> findByNicknameContaining(String nickname, Pageable pageable);
 
     Optional<User> findByIdAndProvider(Long userId, EProvider provider);
 
