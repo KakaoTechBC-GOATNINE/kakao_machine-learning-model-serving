9baef920df8ef13789f39b93c70136cd6552c87d
JJ Lee <maiload77@gmail.com>
2024-07-14 03:08:34 +0900

    #13 feat : Service 클래스에 `@Transactional` 추가

--- server/src/main/java/com/example/kakao_mlms/service/UserService.java
+++ server/src/main/java/com/example/kakao_mlms/service/UserService.java
@@ -6,16 +6,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
+    @Transactional(readOnly = true)
     public Page<UserDto> getAllUsers(Pageable pageable) {
         return userRepository.findAll(pageable).map(UserDto::new);
     }
 
+    @Transactional(readOnly = true)
     public Page<UserDto> getUsersByName(String name, Pageable pageable) {
         return userRepository.findByNickname(name, pageable).map(UserDto::new);
     }
