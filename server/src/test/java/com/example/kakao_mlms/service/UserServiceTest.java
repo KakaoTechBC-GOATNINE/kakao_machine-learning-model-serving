f490d5860ce313efa7b8e970e4d3c77627d44e26
JJ Lee <maiload77@gmail.com>
2024-07-14 21:37:07 +0900

    #13 test : 전체 조회 테스트와 nickname 조회 테스트

---
+++ server/src/test/java/com/example/kakao_mlms/service/UserServiceTest.java
@@ -0,0 +1,53 @@
+package com.example.kakao_mlms.service;
+
+import com.example.kakao_mlms.dto.response.UserDto;
+import com.example.kakao_mlms.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @InjectMocks private UserService sut;
+    @Mock private UserRepository userRepository;
+
+    @Test
+    void searchUsers() {
+        // Given
+        Pageable pageable = Pageable.ofSize(10);
+        given(userRepository.findAll(pageable)).willReturn(Page.empty());
+
+        // When
+        Page<UserDto> userDtos = sut.searchUsers(null, pageable);
+
+        // Then
+        assertThat(userDtos).isEmpty();
+        then(userRepository).should().findAll(pageable);
+
+    }
+
+    @Test
+    void searchUsersByNickname() {
+        // Given
+        Pageable pageable = Pageable.ofSize(10);
+        String nickname = "nickname";
+        given(userRepository.findByNicknameContaining(nickname, pageable)).willReturn(Page.empty());
+
+        // When
+        Page<UserDto> userDtos = sut.searchUsers(nickname, pageable);
+
+        // Then
+        assertThat(userDtos).isEmpty();
+        then(userRepository).should().findByNicknameContaining(nickname, pageable);
+    }
+}
