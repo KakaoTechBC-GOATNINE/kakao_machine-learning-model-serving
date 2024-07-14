164e385080ed15b901a82933cbd36a37130c063e
JJ Lee <maiload77@gmail.com>
2024-07-14 23:04:32 +0900

    #13 feat : Pageable 파라미터에 default 값 추가

--- server/src/main/java/com/example/kakao_mlms/controller/AdminController.java
+++ server/src/main/java/com/example/kakao_mlms/controller/AdminController.java
@@ -6,6 +6,8 @@ import com.example.kakao_mlms.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +21,9 @@ public class AdminController {
     private final UserService userService;
 
     @GetMapping("/users")
-    public ResponseDto<Page<UserDto>> getAllUsers(@RequestParam(required = false) String name, Pageable pageable) {
+    public ResponseDto<Page<UserDto>> getAllUsers(
+            @RequestParam(required = false) String name,
+            @PageableDefault(size = 10, sort = "nickname", direction = Sort.Direction.ASC) Pageable pageable) {
         Page<UserDto> users = userService.searchUsers(name, pageable);
         return ResponseDto.ok(users);
     }
