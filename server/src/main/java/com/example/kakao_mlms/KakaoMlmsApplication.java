bacf766c7b037bfa47bcef7e248df71d29c569df
정진 <maiload77@gmail.com>
2024-07-19 16:54:10 +0900

    #16 feat : JpaAuditing 을 위해 @EnableJpaAuditing 추가

--- server/src/main/java/com/example/kakao_mlms/KakaoMlmsApplication.java
+++ server/src/main/java/com/example/kakao_mlms/KakaoMlmsApplication.java
@@ -2,7 +2,9 @@ package com.example.kakao_mlms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class KakaoMlmsApplication {
 
