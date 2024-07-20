afaa9bb1c162e6eb18ffd0826218d2f1451ab1d5
정진 <maiload77@gmail.com>
2024-07-20 12:52:17 +0900

    #16 feat : 컨트롤러에 관리자 Qna 조회 기능 추가

--- server/src/main/java/com/example/kakao_mlms/controller/AdminController.java
+++ server/src/main/java/com/example/kakao_mlms/controller/AdminController.java
@@ -1,7 +1,10 @@
 package com.example.kakao_mlms.controller;
 
+import com.example.kakao_mlms.domain.type.Category;
+import com.example.kakao_mlms.dto.QnaDtoWithImages;
 import com.example.kakao_mlms.dto.response.UserDto;
 import com.example.kakao_mlms.exception.ResponseDto;
+import com.example.kakao_mlms.service.QnaService;
 import com.example.kakao_mlms.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AdminController {
     private final UserService userService;
+    private final QnaService qnaService;
 
     @GetMapping("/users")
     public ResponseDto<Page<UserDto>> getAllUsers(
@@ -26,5 +30,14 @@ public class AdminController {
             @PageableDefault(size = 10, sort = "nickname", direction = Sort.Direction.ASC) Pageable pageable) {
         Page<UserDto> users = userService.searchUsers(name, pageable);
         return ResponseDto.ok(users);
+    }
+
+    @GetMapping("/qnas")
+    public ResponseDto<Page<QnaDtoWithImages>> getAllQnas(
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) Category Category,
+            @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+        Page<QnaDtoWithImages> qnaDtoWithImages = qnaService.searchQnas(title, Category, pageable);
+        return ResponseDto.ok(qnaDtoWithImages);
     }
 }
--- server/src/test/java/com/example/kakao_mlms/controller/AdminControllerTest.java
+++ server/src/test/java/com/example/kakao_mlms/controller/AdminControllerTest.java
@@ -1,26 +1,29 @@
 package com.example.kakao_mlms.controller;
 
+import com.example.kakao_mlms.KakaoMlmsApplication;
+import com.example.kakao_mlms.service.QnaService;
 import com.example.kakao_mlms.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.BDDMockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AdminController.class)
 class AdminControllerTest {
 
     @Autowired private MockMvc mvc;
     @MockBean private UserService userService;
+    @MockBean private QnaService qnaService;
     private final String BASE_URL = "/api/v1/admins";
 
     @Test
@@ -33,4 +36,17 @@ class AdminControllerTest {
                 .andExpect(status().isUnauthorized());
 //        then(userService).should().searchUsers(eq(null), any(Pageable.class));
     }
+
+    @Test
+    void getAllQnas() throws Exception {
+        // Given
+        given(qnaService.searchQnas(eq(null), eq(null), any(Pageable.class))).willReturn(Page.empty());
+
+        // When & Then
+        mvc.perform(get(BASE_URL + "/qnas"))
+                .andExpect(status().isUnauthorized());
+//        then(qnaService).should().searchQnas(eq(null), eq(null), any(Pageable.class));
+    }
+
+
 }
