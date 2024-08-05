4003e11f53038d6a3d44024675bdc4de1fe6360f
정진 <maiload77@gmail.com>
2024-08-06 00:16:13 +0900

    #17 fix: QnaDto 에 Title 이 누락돼 있어서 추가

--- server/src/main/java/com/example/kakao_mlms/dto/QnaDto.java
+++ server/src/main/java/com/example/kakao_mlms/dto/QnaDto.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
  */
 public record QnaDto(
         Long id,
+        String title,
         String content,
         Category category,
         LocalDateTime createdDate,
@@ -20,11 +21,24 @@ public record QnaDto(
 ) {
     public static QnaDto from(Qna qna) {
         return new QnaDto(qna.getId(),
+                qna.getTitle(),
                 qna.getContent(),
                 qna.getCategory(),
                 qna.getCreatedDate(),
                 qna.getIsAnswer(),
                 qna.getIsBlind(),
                 UserDto.from(qna.getUser()));
+    }
+
+    public static QnaDto of(String title, String content, Category category, Boolean isBlind, UserDto user) {
+        return new QnaDto(null, title, content, category, null, false, isBlind, user);
+    }
+
+    public static QnaDto of() {
+        return QnaDto.of(null, null, null, null, null);
+    }
+
+    public Qna toEntity() {
+        return Qna.builder().title(title).content(content).category(category).isBlind(isBlind).isAnswer(isAnswer).user(user.toEntity()).build();
     }
 }
