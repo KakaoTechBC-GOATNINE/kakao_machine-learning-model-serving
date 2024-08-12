6d7624832f0faf307897c9902250861739a88bf0
Shortood <woobin929@naver.com>
2024-08-12 14:14:00 +0000

    feat: 로그인 성공시 url yml으로 변경

--- server/src/main/java/com/example/kakao_mlms/security/handler/signin/OAuth2LoginSuccessHandler.java
+++ server/src/main/java/com/example/kakao_mlms/security/handler/signin/OAuth2LoginSuccessHandler.java
@@ -9,6 +9,8 @@ import com.example.kakao_mlms.util.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -22,6 +24,12 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
     private final JwtUtil jwtUtil;
     private final UserRepository userRepository;
 
+    @Value("${after-login.oauth2-success}")
+    private String LOGIN_URL;
+
+    @Value("${after-login.oauth2-success-guest}")
+    private String LOGIN_URL_GUEST;
+
     @Override
     @Transactional
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
@@ -34,9 +42,9 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         CookieUtil.addCookie(response, "accessToken", jwtTokenDto.getAccessToken());
 
         if (userPrincipal.getRole() == ERole.GUEST) {
-            response.sendRedirect("http://localhost:5173/sign-up");
+            response.sendRedirect(LOGIN_URL_GUEST);
         } else {
-            response.sendRedirect("http://localhost:5173");
+            response.sendRedirect(LOGIN_URL);
         }
     }
 }
