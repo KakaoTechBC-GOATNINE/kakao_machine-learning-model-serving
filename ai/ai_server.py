ce6f9c3200985e67ca057c71b6da40ad3f9e2cd7
Ilmin Cho <choim426@gmail.com>
2024-08-19 16:39:35 +0900

    docs: requirement 버전 충돌해결

--- ai/ai_server.py
+++ ai/ai_server.py
@@ -4,13 +4,13 @@ from src.data_processing.location_keyword import get_location_name, extract_dong
 from src.data_processing.kakao_review_data_crawling import crawl_restaurant_reviews, save_to_csv
 
 app = FastAPI() # FastAPI 애플리케이션 생성
-
 # 요청 본문 데이터 모델 정의
 class KeywordLocationRequest(BaseModel):
     latitude: float  # 위도
     longitude: float  # 경도
     keyword: str  # 사용자가 검색하는 키워드
 
+# 함수이름도 변경
 @app.post("/api/v1/stores/ai")
 def extract_keyword_and_dong(request: KeywordLocationRequest):
     try:
@@ -23,7 +23,10 @@ def extract_keyword_and_dong(request: KeywordLocationRequest):
         # 리뷰 데이터 크롤링
         reviews = crawl_restaurant_reviews(combined, pages=5) # 최대 5페이지 크롤링
         # 데이터 저장
-        save_to_csv(reviews, 'restaurant_reviews.csv')
+        # ?? 이제필요한가? 필요없을지도
+        save_to_csv(reviews, 'restaurant_reviews.csv') 
+
+
         return {"status": "success", "location": combined, "reviews": reviews}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
