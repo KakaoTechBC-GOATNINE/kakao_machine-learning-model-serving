ee618ccf0da7c73af2ff47dfd833803b932a26e7
정진 <maiload77@gmail.com>
2024-08-21 14:01:14 +0900

    fix: (back) 게시글 상세보기에서 title 데이터를 누락한거 추가

--- server/src/main/java/com/example/kakao_mlms/dto/response/QnaDtoWithImagesResponse.java
+++ server/src/main/java/com/example/kakao_mlms/dto/response/QnaDtoWithImagesResponse.java
@@ -2,12 +2,12 @@ package com.example.kakao_mlms.dto.response;
 
 import com.example.kakao_mlms.domain.type.Category;
 import com.example.kakao_mlms.dto.QnaDtoWithImages;
-import com.example.kakao_mlms.dto.UserDto;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record QnaDtoWithImagesResponse(Long id,
+                                       String title,
                                        String content,
                                        Category category,
                                        LocalDateTime createdDate,
@@ -15,15 +15,18 @@ public record QnaDtoWithImagesResponse(Long id,
                                        Boolean isBlind,
                                        UserDto user,
                                        List<ImageResponse> images) {
+) {
     public static QnaDtoWithImagesResponse from(QnaDtoWithImages qnaDtoWithImages) {
         return new QnaDtoWithImagesResponse(qnaDtoWithImages.id(),
+                qnaDtoWithImages.title(),
                 qnaDtoWithImages.content(),
                 qnaDtoWithImages.category(),
                 qnaDtoWithImages.createdDate(),
                 qnaDtoWithImages.isAnswer(),
                 qnaDtoWithImages.isBlind(),
-                qnaDtoWithImages.user(),
+                UserDtoResponse.from(qnaDtoWithImages.user()),
                 qnaDtoWithImages.images().stream()
                         .map(ImageResponse::from).toList());
+
     }
 }
